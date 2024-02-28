9079868d419ac742fbf7661795d62f07210da19f
KitGlobal <kitapp.developers@gmail.com>
2024-02-28 12:46:02 +0200

    ver 2.1

--- .eslintrc_back-end.js
+++ .eslintrc_back-end.js
@@ -1,30 +1,74 @@
-{ 
-  "env": { 
-    "browser": false, 
-    "es2021": true 
-  }, 
-  "parser": "@typescript-eslint/parser", 
-  "parserOptions": { 
-    "ecmaVersion": "latest", 
-    "sourceType": "module" 
-  }, 
-  "plugins": ["@typescript-eslint"], 
-  "rules": { 
-    "indent": ["warn", 2], 
-    "linebreak-style": ["warn", "windows"], 
-    "quotes": ["warn", "single"], 
-    "semi": ["error", "always"], 
-    "eqeqeq": "off", 
-    "constructor-super": "error", 
-    "no-cond-assign": "error", 
-    "no-constant-binary-expression": "warn", 
-    "no-constant-condition": "warn", 
-    "no-irregular-whitespace": "warn", 
-    "no-this-before-super": "warn", 
-    "func-style": ["error", "declaration", { "allowArrowFunctions": true }], 
-    "max-classes-per-file": ["warn", 1], 
-    "no-else-return": "warn", 
-    "no-return-await": "warn", 
-    "lines-between-class-members": "warn" 
-  } 
+{
+  "env": {
+    "browser": false,
+    "es2021": true
+  },
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": ["@typescript-eslint", "import", "unused-imports"],
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended"
+  ],
+  "rules": {
+    "quotes": ["warn", "single"],
+    "semi": ["error", "always"],
+    "eqeqeq": "off",
+    "constructor-super": "error",
+    "no-cond-assign": "error",
+    "no-constant-binary-expression": "warn",
+    "no-constant-condition": "warn",
+    "no-irregular-whitespace": "warn",
+    "no-this-before-super": "warn",
+    "func-style": [
+      "error",
+      "declaration",
+      {
+        "allowArrowFunctions": true
+      }
+    ],
+    "max-classes-per-file": "off",
+    "no-else-return": "warn",
+    "no-return-await": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "no-console": [
+      "warn",
+      {
+        "allow": ["warn", "error"]
+      }
+    ],
+    "prefer-const": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "unused-imports/no-unused-imports": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-inferrable-types": "off",
+    "import/order": [
+      "error",
+      {
+        "newlines-between": "always",
+        "alphabetize": { "order": "asc", "caseInsensitive": true },
+        "groups": [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "object",
+          "type"
+        ]
+      }
+    ]
+  },
+  "settings": {
+    "import/resolver": {
+      "typescript": true,
+      "node": true
+    }
+  }
 }
--- .eslintrc_front-end.js
+++ .eslintrc_front-end.js
@@ -2,7 +2,16 @@ module.exports = {
   env: {
     jest: true,
   },
-  extends: ['@react-native-community', 'prettier'],
+  plugins: ['import', '@typescript-eslint', 'prettier', 'prefer-arrow'],
+  extends: [
+    '@react-native-community',
+    'prettier',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:@typescript-eslint/recommended',
+  ],
   globals: {
     JSX: true,
     TS: true,
@@ -10,12 +19,34 @@ module.exports = {
   },
   ignorePatterns: ['Pods/', 'acceptableUsePolicy.ts'],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'prettier', 'prefer-arrow'],
   root: true,
   rules: {
+    'react/no-unstable-nested-components': [
+      'off',
+      {
+        allowAsProps: false,
+      },
+    ],
+    'no-empty': 'off',
+    'no-unused-vars': 'off',
+    'no-empty-function': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-duplicates': 'off',
+    'import/named': 'off',
+    'no-extra-boolean-cast': 'off',
+    'no-empty-pattern': 'off',
+
+    'import/no-named-as-default': 'off',
+    'import/no-named-as-default-member': 'off',
+
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-extra-semi': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-empty-function': 'off',
+
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-shadow': 'error',
-    'arrow-body-style': ['error', 'as-needed'],
     'jest/no-disabled-tests': 'off',
     'no-shadow': 'off',
     'no-var': 'error',
@@ -33,5 +64,73 @@ module.exports = {
     'react-hooks/exhaustive-deps': 'off',
     'multiline-ternary': ['error', 'always-multiline'],
     'no-nested-ternary': 'error',
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'always-and-inside-groups',
+        alphabetize: {
+          order: 'asc',
+          orderImportKind: 'asc',
+          caseInsensitive: true,
+        },
+
+        pathGroups: [
+          {
+            pattern: 'react{,-native}',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@assets{,/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/app{,/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/widgets{,/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/features{,/**}',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/entities{,/**}',
+            group: 'internal',
+            position: 'after',
+          },
+          {
+            pattern: '@/shared{,/**}',
+            group: 'internal',
+            position: 'after',
+          },
+        ],
+
+        pathGroupsExcludedImportTypes: ['react{,-native}'],
+
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+      },
+    ],
   },
-};
+  settings: {
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+  },
+}
